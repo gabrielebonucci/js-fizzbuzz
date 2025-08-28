65babd3aca7ce8398dfb8d8549df1c44ebaa5c98
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-08-28 14:08:31 +0200

    Ragionamento logico per lo svolgimento dell'esercizio

---
+++ script.js
@@ -0,0 +1,21 @@
+// CONSEGNA 
+// Scrivi un programma che stampi in console i numeri da 1 a 100.
+// Per i multipli di 3 stampi “Fizz” al posto del numero.
+// Per i multipli di 5 stampi “Buzz”.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+
+// RAGIONAMENTO
+
+// 1. creare un ciclo for per generare i numeri da 1 a 100
+
+// 2. per ogni numero controllare nell'ordine:
+//    a. Che sia multiplo sia di 3 che di 5, se sì, stampa “FizzBuzz”.
+//    b. Altrimenti, è multiplo solo di 3? se sì, stampa "Fizz".
+//    c. Altrimenti, è multiplo solo di 5? se sì, stampa "Buzz"
+//    d. Se non è nessuna delle precedenti, stampa il numero stesso.
+
+// SVOLGIMENT0
+for(let i = 1; i <=100; i++) {
+
+}
